03c36fc27fd35f668d962f202866b46b9643aa51
Kamdzy <28660325+Kamdzy@users.noreply.github.com>
2025-07-11 15:17:42 +0200

    refactor registry functions to use HIVE_ALIAS for hive normalization and replace deprecated methods with enumerateKeys and enumerateValues

--- app/util/reg.js
+++ app/util/reg.js
@@ -1,5 +1,18 @@
 const { execFile } = require('child_process');
-const { HKEY, getValue, listKeys, listValues } = require('registry-js');
+const { HKEY, enumerateKeys, enumerateValues } = require('registry-js');
+
+const HIVE_ALIAS = {
+  HKCU: 'HKEY_CURRENT_USER',
+  HKLM: 'HKEY_LOCAL_MACHINE',
+  HKCR: 'HKEY_CLASSES_ROOT',
+  HKU:  'HKEY_USERS',
+  HKCC: 'HKEY_CURRENT_CONFIG',
+  HKEY_CURRENT_USER: 'HKEY_CURRENT_USER',
+  HKEY_LOCAL_MACHINE: 'HKEY_LOCAL_MACHINE',
+  HKEY_CLASSES_ROOT: 'HKEY_CLASSES_ROOT',
+  HKEY_USERS: 'HKEY_USERS',
+  HKEY_CURRENT_CONFIG: 'HKEY_CURRENT_CONFIG'
+};
 
 function writeRegistryString(hive, keyPath, valueName, value) {
   return new Promise((resolve, reject) => {
@@ -36,32 +49,33 @@ function writeRegistryDword(hive, keyPath, valueName, value) {
 }
 
 function ListRegistryAllValues(hive, key) {
-  const hiveEnum = HKEY[hive];
+  const hiveEnum = HKEY[HIVE_ALIAS[hive] || hive];
   if (!hiveEnum) throw new Error(`Unsupported hive: ${hive}`);
   const normalizedKey = key.replace(/\//g, '\\');
-  return listValues(hiveEnum, normalizedKey).map((v) => v.name);
+  return enumerateValues(hiveEnum, normalizedKey).map((v) => v.name);
 }
 
 function listRegistryAllSubkeys(hive, key) {
-  const hiveEnum = HKEY[hive];
+  const hiveEnum = HKEY[HIVE_ALIAS[hive] || hive];
   if (!hiveEnum) throw new Error(`Unsupported hive: ${hive}`);
 
   const normalizedKey = key.replace(/\//g, '\\');
-  return listKeys(hiveEnum, normalizedKey).map((k) => k.name);
+  return enumerateKeys(hiveEnum, normalizedKey).map((k) => k.name);
 }
 
 function readRegistryInteger(hive, key, valueName) {
-  const hiveEnum = HKEY[hive];
+  const hiveEnum = HKEY[HIVE_ALIAS[hive] || hive];
   if (!hiveEnum) throw new Error(`Unsupported hive: ${hive}`);
   const normalizedKey = key.replace(/\//g, '\\');
-  const val = getValue(hiveEnum, normalizedKey, valueName);
+  const values = enumerateValues(hiveEnum, normalizedKey);
+  const val = values.find(v => v.name === valueName);
   if (!val || (val.type !== 'REG_DWORD' && val.type !== 'REG_QWORD')) return null;
   return Number(val.data);
 }
 
 function readRegistryString(hive, key, valueName) {
   // Normalize hive string to HKEY enum
-  const hiveEnum = HKEY[hive];
+  const hiveEnum = HKEY[HIVE_ALIAS[hive] || hive];
   if (!hiveEnum) throw new Error(`Unsupported hive: ${hive}`);
 
   // Normalize key path: replace '/' with '\'
@@ -69,20 +83,21 @@ function readRegistryString(hive, key, valueName) {
 
   // If valueName is empty string, use '(default)' for PowerShell convention,
   // but registry-js expects '' for default value (just pass '')
-  const val = getValue(hiveEnum, normalizedKey, valueName);
-
+  const values = enumerateValues(hiveEnum, normalizedKey);
+  const val = values.find(v => v.name === valueName);
   if (!val || val.type !== 'REG_SZ') return null; // Only accept string values
 
   return val.data;
 }
 
 function readRegistryStringAndExpand(hive, key, valueName) {
-  const hiveEnum = HKEY[hive];
+  const hiveEnum = HKEY[HIVE_ALIAS[hive] || hive];
   if (!hiveEnum) throw new Error(`Unsupported hive: ${hive}`);
-
+  
   const normalizedKey = key.replace(/\//g, '\\');
 
-  const val = getValue(hiveEnum, normalizedKey, valueName);
+  const values = enumerateValues(hiveEnum, normalizedKey);
+  const val = values.find(v => v.name === valueName);
   if (!val || (val.type !== 'REG_EXPAND_SZ' && val.type !== 'REG_SZ')) return null;
 
   // Expand environment variables if REG_EXPAND_SZ, or just return string
@@ -94,13 +109,13 @@ function readRegistryStringAndExpand(hive, key, valueName) {
 }
 
 function regKeyExists(hive, key) {
-  const hiveEnum = HKEY[hive];
+  const hiveEnum = HKEY[HIVE_ALIAS[hive] || hive];
   if (!hiveEnum) throw new Error(`Unsupported hive: ${hive}`);
 
   const parentKey = key.replace(/\//g, '\\');
   try {
     // Attempt to list subkeys (will throw if the key doesn't exist)
-    listKeys(hiveEnum, parentKey);
+    enumerateKeys(hiveEnum, parentKey);
     return true;
   } catch {
     return false;
